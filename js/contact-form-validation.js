caa17ee7cd6ba1fefd71a25ef1f9d10f26161deb
Maria Romanova <aemulare@users.noreply.github.com>
2017-07-22 22:01:42 -0400

    Adds contact form validation.

---
+++ js/contact-form-validation.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+    $('#contact-us-all').formValidation({
+        framework: 'bootstrap',
+        err: {
+            container: 'tooltip'
+        },
+        icon: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            firstName: {
+                validators: {
+                    notEmpty: {
+                        message: 'The first name is required'
+                    }
+                }
+            },
+            emailAddress: {
+                validators: {
+                    notEmpty: {
+                        message: 'The email is required'
+                    },
+                    emailAddress: {
+                        message: 'The value is not a valid email address'
+                    }
+                }
+            },
+            lastName: {
+                validators: {
+                    notEmpty: {
+                        message: 'The last name is required'
+                    }
+                }
+            },
+            busPhone: {
+                validators: {
+                    digits: {
+                        message: 'The phone number can contain digits only'
+                    },
+                    notEmpty: {
+                        message: 'The phone number is required'
+                    }
+                }
+            },
+            comments: {
+                validators: {
+                    notEmpty: {
+                        message: 'Your message cannot be empty'
+                    },
+                    stringLength: {
+                        max: 500,
+                        message: 'The message must be less than 500 characters'
+                    }
+                }
+            }
+        }
+    });
+});
