f5792431aca0f9e8109adb2c9a849e6bfb2f13a7
Maria Romanova <aemulare@users.noreply.github.com>
2017-07-28 00:25:22 -0400

    Implements local storage for the cart content.

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
 
     // flip coin on 'click'
@@ -17,10 +16,61 @@ $(document).ready(function(){
     var coinQuantity = 0;
     var idClicked, currentPrice, parentCard, coinName, parentCardId, btnIndex;
     var itemQuantities = new Array(1000).fill(0);
+    var cart = [];
+
+
+    function isInCart(coin) {
+        return coin.id === parentCard.prop("id");
+    }
+
+
+    function saveToLocalStorage(cart) {
+        var jsonStr = JSON.stringify(cart);                 // turn JSON object to string
+        localStorage.setItem( "cart", jsonStr );            // store in local storage
+    }
+
+
+    function getFromLocalStorage() {
+        var jsonStr = localStorage.getItem( "cart" );                       // get json string from local storage
+        var parsed = JSON.parse(jsonStr);                                   // parse json string
+        cart = Object.keys(parsed).map(function(k) { return parsed[k] });   // build an array with content from local storage
+        return cart;
+    }
+
+
+    function cartTotal() {
+        var total = 0;
+        var cart = getFromLocalStorage();
+        for (var i = 0; i < cart.length; i++) {
+            var coin = cart[i];
+            total += coin.Quantity * coin.Price;
+        }
+        return total;
+    }
+
+    // local storage test
+    // if (typeof(Storage) !== "undefined") {
+    //     alert("Local storage is set! Local storage length = " + localStorage.length);
+    // }
+    // else {
+    //     alert("Sorry! No Web Storage support..");
+    // }
+
+
+    $('#clear-cart').click(function() {
+        localStorage.clear();
+    });
+
+
+    $('#view').click(function() {
+        var cartString = localStorage.getItem( "cart" ); // get cart JSON object from local storage
+        alert("Cart string in local storage: " + cartString);
+
+    });
 
 
     // on price button click
-    $("button").click(function(e){
+    $("a button").click(function(e){
         idClicked = this.id;
         coinPrice = this.value;
 
@@ -36,28 +86,39 @@ $(document).ready(function(){
         coinDescription = $(parentCardId).find("span").eq(1).text();
         coinImg = $(parentCardId).find("img").eq(0).attr("src");
 
+        // create coin object
+        var coin = {
+            "id":parentCard.prop('id'),
+            "Name":coinName,
+            "Description":coinDescription,
+            "Img":coinImg,
+            "Price":coinPrice,
+            "Quantity":1
+        };
 
-        // if a coin is already in the cart then update only quantity
-        if (coinQuantity === 0) {
-            itemQuantities[btnIndex] += 1;
-            $(".dropdown-menu span").append("<li><table><tr>" +
-                "<td><img class='coin' src='" + coinImg + "'></td>" +
-                "<td>" + coinName + "</td>" +
-                "<td id='"+btnIndex+"'>" + itemQuantities[btnIndex] + "x</td>" +
-                "<td>$" + numberWithCommas(coinPrice) + "</td>" +
-                "</tr></table></li>");
+
+
+        // EMPTY CART
+        if (localStorage.length === 0) {
+            cart.push(coin);                        // add coin to cart
+            saveToLocalStorage(cart);
         }
+
+        // NOT EMPTY CART
         else {
-                itemQuantities[btnIndex] += 1;
-                sel = "#" + btnIndex;
-                $(sel).text(itemQuantities[btnIndex] + "x");
+            cart = getFromLocalStorage();
+            var coinFound = cart.find(isInCart);
+
+            if (coinFound === undefined) {
+                cart.push(coin);                    // add coin to cart
+                saveToLocalStorage(cart);
+            }
+            else {
+                coinFound.Quantity += 1;            // update quantity
+                saveToLocalStorage(cart);
+            }
         }
 
-        // alert("You clicked button: " + idClicked + ", item price = " + coinPrice);
-
-        // total amount in the cart
-        theTotal = Number(theTotal) + Number($(this).val());
-        $("#total").text(numberWithCommas(theTotal.toFixed(2)));
 
         // pop-up confirmation message update
         $(".modal-header").html(
@@ -76,6 +137,35 @@ $(document).ready(function(){
         e.preventDefault();
 
     }); // end click function
+
+
+
+    function renderCart() {
+        cart = getFromLocalStorage();
+
+        // for each coin in the array render an item in shopping cart
+        for (var i = 0; i < cart.length; i++) {
+            var coin = cart[i];
+            $("#cart-content").append("<li><table><tr>" +
+                "<td><img class='coin' src='" + coin.Img + "'></td>" +
+                "<td>" + coin.Name + "</td>" +
+                "<td id='"+coin.id+"'>" + coin.Quantity + "x</td>" +
+                "<td>$" + numberWithCommas(coin.Price) + "</td>" +
+                "</tr></table></li>");
+        }
+
+        // total amount in the cart
+        theTotal = cartTotal();
+        $("#total").text(numberWithCommas(theTotal.toFixed(2)));
+    }
+
+
+    // each time on hover refresh cart from local storage
+    $('.dropdown').hover(function() {
+        $("#cart-content").html("");
+        renderCart();
+    });
+
 
 }); // end ready function
 
